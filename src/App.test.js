35c740274b1c540687986f09270b70e6fbbe8a05
Jared Allen <jallen@extremenetworks.com>
2021-09-30 12:42:59 -0400

    added back test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
+test('internet is present', () => {
+  render(<App />);
+  const internet = screen.getByText("internet");
+  expect(internet).toBeInTheDocument();
+});
 
 // test('renders learn react link', () => {
 //   render(<App />);
